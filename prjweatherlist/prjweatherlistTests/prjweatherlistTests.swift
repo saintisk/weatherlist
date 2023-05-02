46b99802cee4dc388d6690465c7ce9e91b8a149b
SeongInKim <saintiskim@gmail.com>
2023-05-03 01:42:36 +0900

    API 호출 유닛테스트 코드 수정

--- prjweatherlist/prjweatherlistTests/prjweatherlistTests.swift
+++ prjweatherlist/prjweatherlistTests/prjweatherlistTests.swift
@@ -6,11 +6,31 @@
 //
 
 import XCTest
+import RxSwift
 
 final class prjweatherlistTests: XCTestCase {
-
+    
+    var testCoords: [(CGFloat, CGFloat)]!
+    var testMocksWeather: [String]!
+    var testMocksForecast: [String]!
+    
     override func setUpWithError() throws {
         // Put setup code here. This method is called before the invocation of each test method in the class.
+        testCoords = [
+            (37.5665,126.978), //seoul
+            (51.5099,-0.1181), //london
+            (41.8781,-87.6298) //chicago
+        ]
+        testMocksWeather = [
+            "mock_weather_seoul",
+            "mock_weather_london",
+            "mock_weather_chicago"
+        ]
+        testMocksForecast = [
+            "mock_forecast_seoul",
+            "mock_forecast_london",
+            "mock_forecast_chicago"
+        ]
     }
 
     override func tearDownWithError() throws {
@@ -25,48 +45,61 @@ final class prjweatherlistTests: XCTestCase {
         // Mark your test async to allow awaiting for asynchronous code to complete. Check the results with assertions afterwards.
     }
     
-    func testApi() {
+    func testWeatherApi() {
         
-        let issueExpectation = expectation(description: "API Test")
-        issueExpectation.expectedFulfillmentCount = 2
+        let issueExpectation = expectation(description: "testWeatherApi")
+        issueExpectation.expectedFulfillmentCount = testCoords.count
         
-        APIService().requestWeather(lat: 41.8781, lon: -87.6298)
-            .debug()
-            .subscribe { [weak self] event in
-                switch event {
-                case .next(let data):
-                    print("next")
-                case .completed:
-                    print("completed")
-                    issueExpectation.fulfill()
-                case .error(let err):
-                    print("error : \(err)")
-                    XCTFail("testApi fail")
+        testCoords.forEach { coord in
+            
+            APIService().requestWeather(lat: coord.0, lon: coord.1)
+                .debug()
+                .subscribe { [weak self] event in
+                    switch event {
+                    case .next(let data):
+                        print("next")
+                    case .completed:
+                        print("completed")
+                        issueExpectation.fulfill()
+                    case .error(let err):
+                        print("error : \(err)")
+                        XCTFail("testApi fail")
+                    }
                 }
-            }
-        
-        APIService().requestForecast(lat: 41.8781, lon: -87.6298)
-            .debug()
-            .subscribe { [weak self] event in
-                switch event {
-                case .next(let data):
-                    print("next")
-                case .completed:
-                    print("completed")
-                    issueExpectation.fulfill()
-                case .error(let err):
-                    print("error : \(err)")
-                    XCTFail("testApi fail")
-                }
-            }
+        }
         
         wait(for: [issueExpectation], timeout: 5 * Double(issueExpectation.expectedFulfillmentCount))
     }
     
+    func testForecastApi() {
+        
+        let issueExpectation = expectation(description: "testForecastApi")
+        issueExpectation.expectedFulfillmentCount = testCoords.count
+        
+        testCoords.forEach { coord in
+            
+            APIService().requestForecast(lat: coord.0, lon: coord.1)
+                .debug()
+                .subscribe { [weak self] event in
+                    switch event {
+                    case .next(let data):
+                        print("next")
+                    case .completed:
+                        print("completed")
+                        issueExpectation.fulfill()
+                    case .error(let err):
+                        print("error : \(err)")
+                        XCTFail("testApi fail")
+                    }
+                }
+        }
+        wait(for: [issueExpectation], timeout: 5 * Double(issueExpectation.expectedFulfillmentCount))
+    }
+    
     func testWeather() {
-        weatherTestFrom(mockname: "mock_weather_seoul")
-        weatherTestFrom(mockname: "mock_weather_london")
-        weatherTestFrom(mockname: "mock_weather_chicago")
+        weatherTestFrom(mockname: testMocksWeather[0])
+        weatherTestFrom(mockname: testMocksWeather[1])
+        weatherTestFrom(mockname: testMocksWeather[2])
     }
     
     func weatherTestFrom(mockname: String) {
@@ -96,9 +129,9 @@ final class prjweatherlistTests: XCTestCase {
     
     func testForecast() {
         // 테스트 데이터 준비
-        forecastTestFrom(mockname: "mock_forecast_seoul")
-        forecastTestFrom(mockname: "mock_forecast_london")
-        forecastTestFrom(mockname: "mock_forecast_chicago")
+        forecastTestFrom(mockname: testMocksForecast[0])
+        forecastTestFrom(mockname: testMocksForecast[1])
+        forecastTestFrom(mockname: testMocksForecast[2])
     }
     
     func forecastTestFrom(mockname: String) {
